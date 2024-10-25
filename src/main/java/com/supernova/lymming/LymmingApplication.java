511332ae7a2379d2f3569d5f0955ad60f2467ce8
jiurim <jiurim0316@naver.com>
2024-10-25 16:21:12 +0900

    Feat : 리밍 초기세팅 maven

---
+++ src/main/java/com/supernova/lymming/LymmingApplication.java
@@ -0,0 +1,13 @@
+package com.supernova.lymming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LymmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LymmingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supernova/lymming/LymmingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supernova.lymming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LymmingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
