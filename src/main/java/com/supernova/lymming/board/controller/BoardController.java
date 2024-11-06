12edd9f9c353f0181787e9d9daa704190b62e411
jiurim <jiurim0316@naver.com>
2024-11-07 03:21:11 +0900

    Fix : Board Controller CrossOrigin 추가

--- src/main/java/com/supernova/lymming/board/controller/BoardController.java
+++ src/main/java/com/supernova/lymming/board/controller/BoardController.java
@@ -21,24 +21,28 @@ public class BoardController {
     }
 
     @PostMapping("/teambuild")
+    @CrossOrigin(origins = "https://lymming.link", maxAge = 3600)
     public ResponseEntity<BoardDto> createBoard(@RequestBody BoardDto boardDto) {
         BoardDto newBoard = boardService.createBoard(boardDto);
         return ResponseEntity.ok().body(newBoard);
     }
 
     @GetMapping("/participate")
+    @CrossOrigin(origins = "https://lymming.link", maxAge = 3600)
     public ResponseEntity<List<BoardDto>> getBoards() {
         List<BoardDto> boards = boardService.getBoardList();
         return ResponseEntity.ok().body(boards);
     }
 
     @GetMapping("/participate/{projectId}")
+    @CrossOrigin(origins = "https://lymming.link", maxAge = 3600)
     public ResponseEntity<BoardDto> getBoard(@PathVariable Long projectId) {
         BoardDto detailBoard = boardService.getBoardById(projectId);
         return ResponseEntity.ok().body(detailBoard);
     }
 
     @PutMapping("/projectId")
+    @CrossOrigin(origins = "https://lymming.link", maxAge = 3600)
     public ResponseEntity<BoardDto> updateBoard(@PathVariable Long projectId, @RequestBody BoardDto boardDto) {
         BoardDto updateBorad = boardService.update(Math.toIntExact(projectId),boardDto);
         return ResponseEntity.ok().body(updateBorad);
